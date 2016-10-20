9663da3b6c997a3ad1bb76c03293f5675c8e7ca8
Olivier Chafik <olivier.chafik@gmail.com>
2016-10-20 01:41:10 +0100

    tweak rollup config

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import Immutable = require('immutable');
+import * as Immutable from 'immutable';
 
 document.getElementById('content')!.innerHTML = `
   Hello ${Immutable.Set.of(1, 2, 1)}...
--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
 import typescript from 'rollup-plugin-typescript';
 import uglify from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: './main.ts',
@@ -7,10 +10,38 @@ export default {
   dest: 'build/bundle.js',
   sourceMap: true,
   plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'immutable': [
+            'Iterable',
+            'Seq',
+            'Collection',
+            'Map',
+            'OrderedMap',
+            'List',
+            'Stack',
+            'Set',
+            'OrderedSet',
+            'Record',
+            'Range',
+            'Repeat',
+            'is',
+            'fromJS',
+        ],
+      },
+    }),
+    // babel({
+    //   exclude: 'node_modules/**'
+    // }),
     typescript({
-      // Force usage of same version of typescript as the project:
+      // Force usage of same version of typescript as the project:,
       typescript: require('typescript')
     }),
-    uglify({sourceMap: true}),
+    // uglify({sourceMap: true}),
   ]
 }
